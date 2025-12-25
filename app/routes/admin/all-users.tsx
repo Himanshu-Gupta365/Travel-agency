ba59d8fe3c2f96fc753dae31017d85632498938e
Himanshu Gupta <himanshucse49@gmail.com>
2025-12-25 21:01:30 +0530

    Implemented Diff Page layout

---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,17 @@
+import { Header } from 'components'
+import React from 'react'
+
+const AllUsers = () => {
+  return (
+    <main className='dashboard wrapper'>
+        <Header
+       title={`Trips Page`}
+       description="check out our current users in real time"
+      />
+
+      Page contents
+    </main>
+  )
+}
+
+export default AllUsers
